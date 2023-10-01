f98b3c308d58821e2a465c3753a1a97e5b5e3a65
juanjose999 <juanjose000012@gmail.com>
2023-10-01 18:21:53 -0500

    quinto cambio, fondo de color cuando es false o true

--- app.js
+++ app.js
@@ -6,7 +6,8 @@ let num4 = document.getElementById('num4');
 let inputUser = document.querySelector('input');
 let btn = document.getElementById('enviar');
 let contadorElement = document.getElementById('contador');
-let btnClose = document.getElementById('close')
+let btnClose = document.getElementById('close');
+const body = document.body;
 
 // inicializo arrayAleatorio y el total
 let arrayAleatorio;
@@ -40,6 +41,8 @@ let contador = 0;
 generarArrayAleatorio(4);
 actualizarNumeros();
 
+
+
 // le digo al btn cuando escuche el click llame a la funcion verificar
 btn.addEventListener('click', verificar);
 // funcion que compara el valor que ingreso el usuario con el valor la funcion suma
@@ -47,23 +50,51 @@ function verificar() {
     let valorUsuario = parseInt(inputUser.value);
     if (inputUser.value.trim().length === 0 || inputUser.value.trim().length < 2) {
         alert("Valores incorrectos");
+        cambiarColor("red");
         return; // Sale de la función si los valores son incorrectos
+        
     }
     if (valorUsuario === sumarNumerosAleatorios()) {
         alert("Suma correcta");
         generarArrayAleatorio(4);
         actualizarNumeros();
         actualizarContador();
+        cambiarColor("green");
         inputUser.value = "";
     } else {
         alert("Suma incorrecta, vuelve a intentarlo");
+        cambiarColor("red")
     }
 }
-function actualizarContador(){
+function cambiarColor(color) {
+    body.style.background = color;
+    // Revertir el color después de 3 segundos
+    setTimeout(function () {
+        body.style.background = "";
+    }, 600);
+}
+function actualizarContador() {
     contador++;
     contadorElement.textContent = contador.toString();
 }
-btnClose.addEventListener('click',cerrar);
-function cerrar(){
-    alert(`Terminaste el juego \ncon un total de: ${contador} puntos`)
+btnClose.addEventListener('click', cerrar);
+function cerrar() {
+    alert(`Terminaste el juego \ncon un total de: ${contador} puntos`);
+    document.body.classList.add('desenfocado');
+
+    // Quitar la clase después de un tiempo para revertir el desenfoque
+    setTimeout(function () {
+        document.body.classList.remove('desenfocado');
+    }, 300);
 }
+
+// Agregar un listener para el evento click
+btnClose.addEventListener('click', function () {
+    // Agregar la clase de escala al hacer clic
+    btnClose.classList.add('escalar');
+
+    // Quitar la clase después de un tiempo para permitir que la animación se repita
+    setTimeout(function () {
+        btnClose.classList.remove('escalar');
+    }, 300);
+});
